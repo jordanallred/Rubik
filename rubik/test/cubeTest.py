1a76037c44a2ab60a1a212262e2229e676ce0a03
jra0027 <jra0027@Jordan_PC>
2023-02-17 15:17:46 -0600

    Red Light      |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -11,6 +11,6 @@ class Test(unittest.TestCase):
 
 
     def test100_validColorQuantity(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             Cube("bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwwx")
         
