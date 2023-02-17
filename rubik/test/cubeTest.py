f2c8747a78a0a19fdcd678891bfa0906307b1398
jra0027 <jra0027@Jordan_PC>
2023-02-17 15:30:58 -0600

    Red Light      |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -8,9 +8,10 @@ from rubik.model.cube import Cube
 
 
 class Test(unittest.TestCase):
-
-
     def test100_validColorQuantity(self):
         with self.assertRaises(ValueError):
             Cube("bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwwx")
-        
+    
+    def test101_validAlpha(self):
+        with self.assertRaises(ValueError):
+            Cube("bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwww:")        
