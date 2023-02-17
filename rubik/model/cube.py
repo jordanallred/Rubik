f3f1f2d64b4d1b865319ce850e53adb8a25b90cd
jra0027 <jra0027@Jordan_PC>
2023-02-17 15:29:50 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -6,6 +6,12 @@ class Cube:
     '''
 
     def __init__(self, encodedCube):
+        uniqueColors = []
+        for color in encodedCube:
+            if color not in uniqueColors:
+                uniqueColors.append(color)
+        if len(uniqueColors) != 6:
+            raise ValueError("Number of unique colors must be six.")
         self.cube = encodedCube
         
     def rotate(self, directions):
