ebed978747f6af648b5b53744a094ebd0d3c7aa1
jra0027 <jra0027@Jordan_PC>
2023-02-17 15:12:22 -0600

    Red Light      |

---
+++ rubik/test/cubeTest.py
@@ -0,0 +1,15 @@
+'''
+Created on Feb 17, 2023
+
+@author: jra0027
+'''
+import unittest
+from rubik.model.cube import Cube
+
+
+class Test(unittest.TestCase):
+
+
+    def test100_validColorQuantity(self):
+        self.assertRaises(ValueError, Cube("bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwwx")
+        
