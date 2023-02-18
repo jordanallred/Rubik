e5d747bdf8fe09f2813fb6072e804cdf0726c2ab
jra0027 <jra0027@Jordan_PC>
2023-02-17 18:00:55 -0600

    Green Light    |

--- rubik/model/cube.py
+++ rubik/model/cube.py
@@ -38,7 +38,83 @@ class Cube:
         if len(directions):
             directions = "F"
         
+        while len(directions) > 0:
+            if directions[0] == 'F':
+                self._rotateFrontClockwise()
+            elif directions[0] == 'f':
+                self._rotateFrontCounterclockwise()
+            elif directions[0] == 'B':
+                self._rotateBackClockwise()
+            elif directions[0] == 'b':
+                self._rotateBackCounterclockwise()
+            elif directions[0] == 'L':
+                self._rotateLeftClockwise()
+            elif directions[0] == 'l':
+                self._rotateLeftCounterclockwise()
+            elif directions[0] == 'R':
+                self._rotateRightClockwise()
+            elif directions[0] == 'r':
+                self._rotateRightCounterclockwise()
+            elif directions[0] == 'U':
+                self._rotateUpClockwise()
+            elif directions[0] == 'u':
+                self._rotateUpCounterclockwise()
+            
+            directions = directions[1:] if len(directions) > 1 else []
+        
     
     def get(self):
         return self.cube
+    
+    def _rotateFrontClockwise(self):
+        cubeCopy = self.cube.copy()
+        # face values
+        self.cube[0] = cubeCopy[6]
+        self.cube[1] = cubeCopy[3]
+        self.cube[2] = cubeCopy[0]
+        self.cube[3] = cubeCopy[7]
+        self.cube[5] = cubeCopy[1]
+        self.cube[6] = cubeCopy[8]
+        self.cube[7] = cubeCopy[5]
+        self.cube[8] = cubeCopy[2]
         
+        # edge values
+        self.cube[9] = cubeCopy[42]
+        self.cube[12] = cubeCopy[43]
+        self.cube[15] = cubeCopy[44]
+        self.cube[45] = cubeCopy[15]
+        self.cube[46] = cubeCopy[12]
+        self.cube[47] = cubeCopy[9]
+        self.cube[29] = cubeCopy[45]
+        self.cube[32] = cubeCopy[46]
+        self.cube[35] = cubeCopy[47]
+        self.cube[42] = cubeCopy[35]
+        self.cube[43] = cubeCopy[32]
+        self.cube[44] = cubeCopy[29]
+ 
+    def _rotateFrontCounterclockwise(self):
+        pass
+    
+    def _rotateBackClockwise(self):
+        pass
+    
+    def _rotateBackCounterclockwise(self):
+        pass
+
+    def _rotateLeftClockwise(self):
+        pass
+    
+    def _rotateLeftCounterclockwise(self):
+        pass
+    
+    def _rotateRightClockwise(self):
+        pass
+    
+    def _rotateRightCounterclockwise(self):
+        pass
+    
+    def _rotateUpClockwise(self):
+        pass
+    
+    def _rotateUpCounterclockwise(self):
+        pass
