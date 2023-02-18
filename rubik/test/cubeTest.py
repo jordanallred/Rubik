a57cd0160450554f46bbe741d185f74b48a7343f
jra0027 <jra0027@Jordan_PC>
2023-02-17 20:12:29 -0600

    Green Light    |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -128,6 +128,6 @@ class Test(unittest.TestCase):
         
     def test122_rotateAll(self):
         test = Cube("bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwww")
-        test.rotate("FBLRfblr")
+        test.rotate("FBLRrlbf")
         self.assertEquals(test.get(), "bbbbbbbbbrrrrrrrrrooooooooogggggggggyyyyyyyyywwwwwwwww")
     
