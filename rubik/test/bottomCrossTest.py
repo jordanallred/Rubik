75caaf248b4bebca7a9167e1e2ef0e031b369c17
jordanallred <jra0027@auburn.edu>
2023-03-25 19:19:59 -0500

    Green Light    |

--- rubik/test/bottomCrossTest.py
+++ rubik/test/bottomCrossTest.py
@@ -6,18 +6,39 @@ from rubik.test.tools import create_cube
 
 
 class BottomCrossTest(TestCase):
-    def test101_solve_createDaisy(self):
+    def test101_solve_createDaisy01(self):
         cube = create_cube()
         solution = createDaisy(cube)
         cube.rotate(solution)
         encodedCube = cube.get()
                 
         self.assertEqual(encodedCube[DMM], encodedCube[UTM])
+
+    def test102_solve_createDaisy02(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[UML])
+
+    def test103_solve_createDaisy03(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[UMR])
+
+    def test104_solve_createDaisy04(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[UBM])
     
-    def test102_solve_createBottomCross(self):
+    def test105_solve_bottomCross01(self):
         cube = create_cube()
         solution = createDaisy(cube)
         cube.rotate(solution)
@@ -26,15 +47,82 @@ class BottomCrossTest(TestCase):
         cube.rotate(solution)
         encodedCube = cube.get()
                 
-        # checking centers matching palette underneath
         self.assertEqual(encodedCube[FMM], encodedCube[FBM])
+
+    def test106_solve_bottomCross02(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[RMM], encodedCube[RBM])
+
+    def test107_solve_bottomCross03(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[BMM], encodedCube[BBM])
+
+    def test108_solve_bottomCross04(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[LMM], encodedCube[LBM])
 
-        # checking bottom cross is formed
+    def test109_solve_bottomCross05(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[DTM])
+        
+    def test110_solve_bottomCross06(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[DML])
+
+    def test111_solve_bottomCross07(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[DMR])
+
+    def test112_solve_bottomCross08(self):
+        cube = create_cube()
+        solution = createDaisy(cube)
+        cube.rotate(solution)
+        
+        solution = createBottomCross(cube)
+        cube.rotate(solution)
+        encodedCube = cube.get()
+                
         self.assertEqual(encodedCube[DMM], encodedCube[DBM])
         
