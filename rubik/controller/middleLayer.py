38886607c2fadc340878aa2776acebcc72853ffd
jra0027 <jra0027@Jordan_PC>
2023-04-07 10:58:54 -0500

    Green Light    |

--- rubik/controller/middleLayer.py
+++ rubik/controller/middleLayer.py
@@ -1,12 +1,80 @@
-import rubik.model.constants
+from rubik.model.constants import *
 from rubik.model.cube import Cube
 
+solution = ''
+
 def solveMiddleLayer(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube so that the middle layer is solved.
+    global solution
+    encodedCube = theCube.get()
+    
+    counter = 0
+    while not middleSolved(encodedCube):
+        stuck = False
+        stuck = stuck or moveColors(theCube, FTM, UBM, FRONT)
+        stuck = stuck or moveColors(theCube, RTM, UMR, RIGHT)
+        stuck = stuck or moveColors(theCube, BTM, UTM, BACK)
+        stuck = stuck or moveColors(theCube, LTM, UML, LEFT)
         
-        input:  an instance of the cube class with the bottom layer solved
-        output: the rotations required to solve the middle layer  
-    '''  
-    return ''      #TODO:  remove this stubbed value
+        if stuck:
+            theCube.right_trigger(FRONT)
+            counter += 1
+        else:
+            counter = 0
+            
+        if counter == 4:
+            solution += theCube.right_trigger(FRONT)
+            theCube.rotate('u')
+            solution += 'u'
+            theCube.left_trigger(RIGHT)
+
+        encodedCube = theCube.get()
+        
+    return solution
+    
+def middleSolved(encodedCube) -> bool:    
+    if encodedCube[FML] !=  encodedCube[FMM]:
+        return False
+    if encodedCube[FMR] !=  encodedCube[FMM]:
+        return False
+    
+    if encodedCube[RML] !=  encodedCube[RMM]:
+        return False
+    if encodedCube[RMR] !=  encodedCube[RMM]:
+        return False
+    
+    if encodedCube[BML] !=  encodedCube[BMM]:
+        return False
+    if encodedCube[BMR] !=  encodedCube[BMM]:
+        return False
+        
+    if encodedCube[LML] !=  encodedCube[LMM]:
+        return False
+    if encodedCube[LMR] !=  encodedCube[LMM]:
+        return False
+    
+    return True
+
+def moveColors(theCube: Cube, frontTopMiddle: int, topBottomMiddle: int, side: Face):
+    global solution
+    encodedCube = theCube.get()
+    
+    if encodedCube[frontTopMiddle] == encodedCube[frontTopMiddle + 3]:
+        if encodedCube[topBottomMiddle] == encodedCube[(frontTopMiddle + 3 + (NUM_ELEMENTS // NUM_FACES)) % (4 * (NUM_ELEMENTS // NUM_FACES))]:
+            theCube.rotate('U')
+            solution += 'U'
+            solution += theCube.right_trigger(side)
+            theCube.rotate('u')
+            solution += 'u'
+            theCube.left_trigger(side.right)
+            encodedCube = theCube.get()
+            return True
+        if encodedCube[topBottomMiddle] == encodedCube[(frontTopMiddle + 3 + (NUM_ELEMENTS // NUM_FACES)) % (4 * (NUM_ELEMENTS // NUM_FACES))]:
+            theCube.rotate('u')
+            solution += 'u'
+            solution += theCube.left_trigger(side)
+            theCube.rotate('U')
+            solution += 'U'
+            theCube.right_trigger(side.left)
+            encodedCube = theCube.get()
+            return True
+        return False
