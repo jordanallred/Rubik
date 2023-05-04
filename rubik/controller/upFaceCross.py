f52b2f69ce209a28b2d7e1778ebefc7da2ab27ef
jra0027 <jra0027@Jordan_PC>
2023-05-04 17:41:42 -0500

    Red Light      |

--- rubik/controller/upFaceCross.py
+++ rubik/controller/upFaceCross.py
@@ -8,9 +8,38 @@ def solveUpCross(theCube: Cube) -> str:
     encodedCube = theCube.get()
     
     while not middleSolved(encodedCube):
-        pass
-    
+        if isFish(encodedCube):
+            while encodedCube[UBL] != encodedCube[UMM]:
+                theCube.rotate('U')
+                solution += 'U'
+                encodedCube = theCube.get()
+
+        theCube.rotate('FURurf')
+        solution += 'FURurf'
+        encodedCube = theCube.get()
+        
     return solution
     
-def middleSolved(encodedCube) -> bool:        
+def middleSolved(encodedCube) -> bool:
+    if encodedCube[UMM] != encodedCube[UTM]:
+        return False      
+    if encodedCube[UMM] != encodedCube[UML]:
+        return False      
+    if encodedCube[UMM] != encodedCube[UMR]:
+        return False      
+    if encodedCube[UMM] != encodedCube[UBM]:
+        return False      
+
     return True
+
+def isFish(encodedCube) -> bool:
+    if encodedCube[UTL] == encodedCube[UMM]:
+        return True
+    if encodedCube[UTR] == encodedCube[UMM]:
+        return True    
+    if encodedCube[UBL] == encodedCube[UMM]:
+        return True
+    if encodedCube[UBR] == encodedCube[UMM]:
+        return True
+    
+    return False
