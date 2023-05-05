5444007677d268802ff516ade05be353f579d5f5
jra0027 <jra0027@Jordan_PC>
2023-05-04 19:03:03 -0500

    Green Light    |

--- rubik/controller/upperLayer.py
+++ rubik/controller/upperLayer.py
@@ -1,12 +1,18 @@
 import rubik.model.constants
 from rubik.model.cube import Cube
 
+solution = ''
+
 def solveUpperLayer(theCube: Cube) -> str:
-    '''
-        This is the top-level function  for rotating
-        a cube so that the entire upper layer is solved.
-        
-        input:  an instance of the cube class with up-face surface solved
-        output: the rotations required to solve the upper layer  
-    '''  
-    return ''      #TODO:  remove this stubbed value
+    global solution
+    
+    encodedCube = theCube.get()
+    
+    solution += solveCorners(encodedCube)
+    solution += solveLayer(encodedCube)
+
+def solveCorners(encodedCube) -> str:
+    pass
+
+def solveLayer(encodedCube) -> str:
+    pass
